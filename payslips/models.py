2e740175969ca291892e9d0799c21278ebe9ca61
Guyodub <guyodubjarso@gmail.com>
2021-10-19 04:23:23 +0300

    added new user profile feature

--- payslips/models.py
+++ payslips/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.urls import reverse # new
+
 
 class UserProfile(models.Model):
     name = models.CharField(max_length = 200)
@@ -15,4 +17,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self): # new
+        return reverse('pay_detail', args=[str(self.id)])
 
